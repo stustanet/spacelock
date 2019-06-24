ea8dd590550407d1a9d55d88b9ff8dfef08278e6
Michael Loiführer <michael.loipfuehrer@tum.de>
2019-06-24 21:05:10 +0200

    working bluetooth

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,24 +1,32 @@
 function sendToken(token) {
-  const options = {filters: [{services: 'space_lock'}]};
+  const serviceUUID = '12345678-1234-5678-1234-56789abcdef0';
+  const characteristicUUID = '12345678-1234-5678-1234-56789abcdef5';
+  const options = {
+    filters: [{
+      name: 'X1'
+    }],
+    optionalServices: [
+      serviceUUID
+    ]
+  };
 
   navigator.bluetooth.requestDevice(options)
-      .then(device => {
-        console.log('> Name:             ' + device.name);
-        console.log('> Id:               ' + device.id);
-        console.log('> Connected:        ' + device.gatt.connected);
-        return device.gatt.connect();
-      })
-      .then(server => server.getPrimaryService('space_lock'))
-      .then(service => service.getCharacteristic('space_lock_token'))
-      .then(characteristic => {
-        // Writing 1 is the signal to reset energy expended.
-        // const token = Uint8Array.of(1);
-        return characteristic.writeValue(token);
-      })
-      .then(_ => {
-        console.log('Energy expended has been reset.');
-      })
-      .catch(error => {
-        console.log('Argh! ' + error);
-      });
+    .then(device => {
+      console.log('> Name:             ' + device.name);
+      console.log('> Id:               ' + device.id);
+      console.log('> Connected:        ' + device.gatt.connected);
+      return device.gatt.connect();
+    })
+    .then(server => server.getPrimaryService(serviceUUID))
+    .then(service => service.getCharacteristic(characteristicUUID))
+    .then(characteristic => {
+      const t = Uint8Array.from(atob(token), c => c.charCodeAt(0));
+      return characteristic.writeValue(t);
+    })
+    .then(_ => {
+      console.log('Successfully sent token:', token);
+    })
+    .catch(error => {
+      console.log('Argh! ' + error);
+    });
 }
