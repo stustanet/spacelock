34f15c472fbd6c4b2e317ca1f4d6f8c353e8e7b2
Wolfgang Walter <wolfgang.walter@stusta.de>
2024-04-01 23:47:50 +0200

    more views

--- database/setup_views.sql
+++ database/setup_views.sql
@@ -16,8 +16,14 @@ select system_id, key_id, key_type, secret_key, verify_key
 from signing_keys
 where is_active = true;
 
+create or replace view signing_key_pub_data
+as
+select system_id, key_id, key_type, verify_key, is_active
+from signing_keys;
+
 create or replace view active_signing_key_pub_data
 as
-select system_id, key_id, key_type, verify_key
-from signing_keys
+select *
+from signing_key_pub_data
 where is_active = true;
+
